bc1fdb42fd0a7abcee5858aab57f29c7ae52ea0c
Uchenna99 <uchennaagbu@gmail.com>
2025-02-18 15:09:19 +0100

    updating authentication

--- src/routes/loan.routes.ts
+++ src/routes/loan.routes.ts
@@ -1,14 +1,15 @@
 import express from "express"
 import { LoanController } from "../controllers/loan.controller";
+import { authenticateUser } from "../middleware/userAuth.middleware";
 
 
 const loanRouter = express.Router();
 const loanController = new LoanController();
 
 
-loanRouter.post('/new-loan', loanController.createLoan);
+loanRouter.post('/new-loan', authenticateUser, loanController.createLoan);
 
-loanRouter.get('/get-loans/:id', loanController.getLoanById);
+loanRouter.get('/get-loans/:id', authenticateUser, loanController.getLoanById);
 
 
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { UserController } from "../controllers/user.controller";
+import { authenticateUser } from "../middleware/userAuth.middleware";
 
 
 const userRouter = express.Router();
@@ -8,15 +9,15 @@ const userConroller = new UserController();
 
 userRouter.post('/signup', userConroller.createUser);
 
-userRouter.post('/new-card', userConroller.createCard);
+userRouter.post('/new-card', authenticateUser, userConroller.createCard);
 
-userRouter.post('/new-bank-account', userConroller.createBankAccount);
+userRouter.post('/new-bank-account', authenticateUser, userConroller.createBankAccount);
 
-userRouter.get('/get-bank-accounts/:id', userConroller.getBankAccounts);
+userRouter.get('/get-bank-accounts/:id', authenticateUser, userConroller.getBankAccounts);
 
 userRouter.get('/get-banks-list', userConroller.getBanksList);
 
-userRouter.get('/get-cards-list/:id', userConroller.getPaymentCards);
+userRouter.get('/get-cards-list/:id', authenticateUser, userConroller.getPaymentCards);
 
 
 
